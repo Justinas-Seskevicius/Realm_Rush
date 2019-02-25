e6a3d48f1658e5b54b0fead2a35eaaf70197d90b
zeFerret <justinas.seskevicius@gmail.com>
2019-02-25 23:03:42 +0200

    feat: asset package & turret target follow & enemy hitpoints + collision + death

---
+++ Assets/Scripts/EnemyCollider.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyCollider : MonoBehaviour
+{
+    [SerializeField] int hitPoints = 5;
+    [SerializeField] GameObject deathVFX;
+
+    private void OnParticleCollision(GameObject other)
+    {
+        hitPoints--;
+        if(hitPoints <= 0)
+        {
+            Instantiate(deathVFX, transform.position, Quaternion.identity);
+            Destroy(gameObject);
+        }
+    }
+}
